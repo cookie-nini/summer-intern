17f2682a8e3befbb85e12e84a41c4f268370ce75
cookie-cookie <Qi.Hou2@PERKINELMER.COM>
2018-08-10 17:23:09 +0800

    add edit moudle

--- ES6-learning/bbs/src/PostItem.js
+++ ES6-learning/bbs/src/PostItem.js
@@ -1,41 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './PostItem.css';
 import like from './img/thumb.png';
+import './PostItem.css';
 
 export default class PostItem extends React.Component{
     constructor(props){
         super(props);
-        this.handleClick=this.handleClick.bind(this);
+        this.state={
+            editing:false,
+            post:props.post
+        };
+        this.handleEditPost=this.handleEditPost.bind(this);
+        this.handleTitleChange=this.handleTitleChange.bind(this);
     }
 
-    handleClick(){
-        this.props.onVote(this.props.post.id);
+    componentWillReceiveProps(nextProps){
+        if(this.state.post!==nextProps.post){
+            this.setState({
+                post:nextProps.post
+            });
+        }
+    }
+
+    handleEditPost(){
+        if(this.state.editing){
+            this.props.onSave({...this.state.post})
+        }
+        this.setState({
+            editing:!this.state.editing
+        });
+    }
+
+    handleTitleChange(event){
+        const newPost={...this.state.post,title:event.target.value};
+        this.setState({post:newPost});
     }
 
     render(){
-        const {post}=props;
+        const {post}=this.state; 
         return(
             <li className='item'>
-                <div className='title'>{post.title}</div>
+                <div className='title'>
+                    {this.state.editing?
+                        <textarea value={post.title} onChange={this.handleTitleChange}
+                        />:post.title}
+                </div>
                 <div>创建人： {post.author}</div>
                 <div>创建时间： {post.date}</div>
                 <div className='like'>
-                    <span><img src={like} alt=''onClick={this.handleClick}/></span>
+                    <span><img src={like} alt='' onClick={()=>{this.props.onVote(post.id)}}/></span>
                     <span>{post.vote}</span>
                 </div>
-            </li>   //试着去实现点赞变色
-        )
+                <button onClick={this.handleEditPost}>{this.state.editing?'保存':'编辑'}</button>
+            </li>
+        );
     }
 }
 
-PostItem.propType={
-    post:PropTypes.shape({
-        id:PropTypes.number,
-        title:PropTypes.string,
-        author:PropTypes.string,
-        date:PropTypes.string,
-        vote:PropTypes.number
-    }).isRequired,
-    onVote:PropTypes.func.isRequired
-}
--- ES6-learning/bbs/src/PostList.js
+++ ES6-learning/bbs/src/PostList.js
@@ -1,57 +1,65 @@
 import React from 'react';
 import PostItem from './PostItem';
-import './PostList.css'
+import './PostList.css';
 
 export default class PostList extends React.Component{
-	constructor(props){
-		super(props);
-		this.state={
+    constructor(props){
+        super(props);
+        this.state={
             posts:[]
         };
         this.timer=null;
-		this.handleVote=this.handleVote.bind(this);
-	}
+        this.handleVote=this.handleVote.bind(this);
+        this.handleSave=this.handleSave.bind(this);
+    }
 
     componentDidMount(){
         this.timer=setTimeout(()=>{
             this.setState({
-               posts:[
-                    {id:1,title:'大家一起来学React吧',author:'张三',date:'2017-09-01 10:00',vote:0},
-                    {id:2,title:'前端框架，你最爱哪一个',author:'李四',date:'2017-09-01 12:00',vote:0},
-                    {id:3,title:'Web App时代已经到来',author:'王五',date:'2017-09-01 14:00',vote:0}
+                posts:[
+                    {id:1,title:'大家一起学react吧',author:'张三',date:'2017-09-20 10:00',vote:0},
+                    {id:2,title:'前端框架，你最爱哪个',author:'李四',date:'2017-09-20 12:00',vote:0},
+                    {id:3,title:'web app时代已经到来',author:'王五',date:'2017-09-20 14:00',vote:0}
                 ]
             })
         },1000);
     }
 
-    componentWillUnmount(){
-        if(this.timer){
-            clearTimeout(this.timer);
-        }
+    handleVote(id){
+        const newItem=this.state.posts.filter(item=>item.id===id)[0];
+        newItem.vote++; //newItem就是this.state.posts对应的那项，++之后这项也变了
+        this.setState({posts:this.state.posts}); //设为最新的this.state.posts
     }
 
-	handleVote(id){
-		const {posts}=this.state;
-		const newItem=posts.filter(item=>(item.id===id))[0];
-		newItem.vote++;
-		this.setState({posts});
-	}
+    handleSave(post){
+        const newTitle=this.state.posts.filter(item=>item.id===post.id)[0];
+        newTitle.title=post.title;
+        this.setState({
+            posts:this.state.posts
+        });
+    }
 
-	render(){
-		return(
-		<div className='container'>
-			<h2>话题列表</h2>
-			<ul>
-				{this.state.posts.map(item=>
-                    <PostItem key={item.id} post={item} onVote={this.handleVote}/>
-                )}
-			</ul>
-		</div>
-		);
-	}
+    render(){
+        return(
+            <div className='container'>
+                <h2>话题列表</h2>
+                <ul>
+                    {this.state.posts.map(item=>
+                        <PostItem 
+                            key={item.id} 
+                            post={item} 
+                            onVote={this.handleVote}
+                            onSave={this.handleSave}
+                        />)
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
 
 
 
 
 
+  
